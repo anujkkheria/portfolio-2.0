86be2d9faa658849bb512306eb02f35e522c5f2e
Anuj Kumar Kheria <Anuj.Kheria@insightdelivered.com>
2023-08-12 20:51:42 +0530

    Text

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,16 @@ const App:React.FC = () => {
   return (
     <div className='container h-screen'>
       <Header />
-      <div className='hero container flex content-center items-center justify-center h-[100%]'>
-       <div className='img-container h-[40%] w-[80%] relative flex justify-center items-center '>
+      <div className='hero container flex flex-col gap-4 content-center items-center justify-center h-[100%]'>
+       <div className='img-container h-[40%] w-[80%] sm:h-50 sm:w-80  relative flex justify-center items-center '>
         <div className='img-bg h-[100%] w-[100%] absolute z-0 flex rounded-full  content-center '>
         </div>
           <img src={anuj} alt="anuj" className='h-[98%] w-[98%] z-10 p-1 rounded-full' />
        </div>
+       <h1 className='gradient-text Welcome'>Welcome</h1>
+      <p className=' ml-[50%] font-bold text-xl'>
+         I am Anuj Kumar Kheria, I am a Web developer, designer.
+        </p>
         </div>
     </div>
   )

4ae8061b3592b672c7f245f48c45cdc93ea0aaf8
Anuj Kumar Kheria <Anuj.Kheria@insightdelivered.com>
2023-08-12 17:34:45 +0530

    initial commit

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Menu} from '@mui/icons-material'
 import Logo from './assets/Free_Sample_By_Wix.jpg'
 import { AppBar,IconButton } from '@mui/material'
-const Header = () => {
+const Header:React.FC = () => {
   return (
     <AppBar sx={{display:'flex',flexDirection:"row",justifyContent:"space-between",alignContent:'center', height:45, backgroundColor:"#010101"}}>
         <img src={Logo} alt="Logo" width={"15%"} height={'20%'} className='self-center'/>
